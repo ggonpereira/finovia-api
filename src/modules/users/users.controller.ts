d2cf202ad96ce149077a7daac1482a562f4a1c47
Gabriel Pereira <ggonpereira@gmail.com>
2023-06-27 20:02:57 -0300

    🎉 feat: configure global jwt guard

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('/me')
+  me(@Req() request: Request) {
+    console.log((request as any).userId);
+    return this.usersService.getUserById('userId');
+  }
 }
